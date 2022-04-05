c572ae921e46db54e0e5d1036574d89866262f25
Bogdan Mihaila <mihailabogdan2909@gmail.com>
2022-04-05 15:37:01 +0300

    create a sample using typing indicator for group channel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import BasicOpenChannelSample from './samples/BasicOpenChannelSample';
 import BasicGroupChannelSample from './samples/BasicGroupChannelSample';
+import GroupChannelTypingIndicatorSample from './samples/GroupChannelTypingIndicatorSample';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/open-channel' element={<BasicOpenChannelSample />} />
         <Route path='/group-channel' element={<BasicGroupChannelSample />} />
+        <Route path='/group-channel-typing-indicator' element={<GroupChannelTypingIndicatorSample />} />
 
       </Routes>
     </div>
