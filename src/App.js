80ca7376047fe7c5c0d56730fd2d33b1298a8302
aapk <aapk.upwork@gmail.com>
2022-04-27 14:53:22 +0300

    Create user mentions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BasicOpenChannelSample from './samples/BasicOpenChannelSample';
 import BasicGroupChannelSample from './samples/BasicGroupChannelSample';
 import GroupChannelTypingIndicatorSample from './samples/GroupChannelTypingIndicatorSample';
 import FreezeOpenChannelSample from './samples/FreezeOpenChannelSample';
+import MentionOpenChannelSample from './samples/MentionsOpenChannelSample';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='/group-channel' element={<BasicGroupChannelSample />} />
         <Route path='/group-channel-typing-indicator' element={<GroupChannelTypingIndicatorSample />} />
         <Route path='/freeze-open-channel' element={<FreezeOpenChannelSample />} />
+        <Route path='/mention-open-channel' element={<MentionOpenChannelSample />} />
       </Routes>
     </div>
   );
