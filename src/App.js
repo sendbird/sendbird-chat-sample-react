d1f55d597199ce462a9cb49b3a87ffa73e16641b
aapk <aapk.upwork@gmail.com>
2022-05-03 13:23:39 +0300

    added admin message view

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BasicOpenChannelSample from './samples/BasicOpenChannelSample';
 import BasicGroupChannelSample from './samples/BasicGroupChannelSample';
 import GroupChannelTypingIndicatorSample from './samples/GroupChannelTypingIndicatorSample';
 import FreezeOpenChannelSample from './samples/FreezeOpenChannelSample';
+import OpenChannelSendAnAdminMessage from './samples/OpenChannelSendAnAdminMessage'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='/group-channel' element={<BasicGroupChannelSample />} />
         <Route path='/group-channel-typing-indicator' element={<GroupChannelTypingIndicatorSample />} />
         <Route path='/freeze-open-channel' element={<FreezeOpenChannelSample />} />
+        <Route path='/open-channel-send-an-admin-message' element={<OpenChannelSendAnAdminMessage />} />
       </Routes>
     </div>
   );
