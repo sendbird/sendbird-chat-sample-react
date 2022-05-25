1fcc557d99d0672f9cc4eab55ec6d89a97ab8dd4
AAPK-code <aapk.upwork@gmail.com>
2022-05-25 10:43:48 +0300

    Created sample for mute unmute users in group channels

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ import GroupChannelCategorizeMessagesByCustomType  from './samples/GroupChannelC
 import GroupChannelRegisterUnregisterOperator from './samples/GroupChannelRegisterUnregisterOperator';
 import GroupChannelLocalCaching from './samples/GroupChannelLocalCaching';
 import GroupChannelArchive from './samples/GroupChannelArchive'
+import GroupChannelMuteUnmuteUsers from './samples/GroupChannelMuteUnmuteUsers';
 
 const App = () => {
   return (
@@ -62,6 +63,7 @@ const App = () => {
         <Route path='/group-channel-categorize-messages-by-custom-type' element={<GroupChannelCategorizeMessagesByCustomType />} />
         <Route path='/group-channel-register-unregister-operator' element={<GroupChannelRegisterUnregisterOperator />} />
         <Route path='/group-channel-archive' element={<GroupChannelArchive />} />
+        <Route path='/group-channel-mute-unmute-users' element={<GroupChannelMuteUnmuteUsers />} />
       </Routes>
     </div>
   );
