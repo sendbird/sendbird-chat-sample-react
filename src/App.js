c658fc7ad955602469693cd04e4bb077072d88ab
aapk <aapk.upwork@gmail.com>
2022-06-01 13:13:06 +0300

    added 'show online/offline status of users' functionality for open and group channels

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import OpenChannelSendAndReceiveVariousTypesOfFiles from './samples/OpenChannelS
 import OpenChannelCopyMessage from './samples/OpenChannelCopyMessage';
 import OpenChannelDisplayOGTags from './samples/OpenChannelDisplayOGTags';
 import OpenChannelReportAMessageUserChannel from './samples/OpenChannelReportAMessageUserChannel'
-import OpenChannelCategorizeMessagesByCustomType  from './samples/OpenChannelCategorizeMessagesByCustomType';
+import OpenChannelCategorizeMessagesByCustomType from './samples/OpenChannelCategorizeMessagesByCustomType';
+import OpenChannelUsersOnlineStatus from './samples/OpenChannelUsersOnlineStatus';
 
 // Group channel samples
 import GroupChannelTypingIndicatorSample from './samples/GroupChannelTypingIndicatorSample';
@@ -38,6 +39,7 @@ import GroupChannelBanUnbanUsers from './samples/GroupChannelBanUnbanUsers';
 import GroupChannelOperatorsList from './samples/GroupChannelOperatorsList';
 import GroupChannelMembersListOrder from './samples/GroupChannelMembersListOrder';
 import GroupChannelRetrieveAListOfBannedOrMutedUsers from './samples/GroupChannelRetrieveAListOfBannedOrMutedUsers';
+import GroupChannelUsersOnlineStatus from './samples/GroupChannelUsersOnlineStatus';
 
 const App = () => {
   return (
@@ -75,7 +77,9 @@ const App = () => {
         <Route path='/group-channel-bun-unban-users' element={<GroupChannelBanUnbanUsers />} /> 
         <Route path='/group-channel-operators-list' element={<GroupChannelOperatorsList />} />
         <Route path='/group-channel-members-list-order' element={<GroupChannelMembersListOrder />} />
-        <Route path='/group-channel-retrieve-banned-or-muted-users' element={<GroupChannelRetrieveAListOfBannedOrMutedUsers />} /> 
+        <Route path='/group-channel-retrieve-banned-or-muted-users' element={<GroupChannelRetrieveAListOfBannedOrMutedUsers />} />
+        <Route path='/group-channel-users-online-status' element={<GroupChannelUsersOnlineStatus />} />
+        <Route path='/open-channel-users-online-status' element={<OpenChannelUsersOnlineStatus />} />
       </Routes>
     </div>
   );
