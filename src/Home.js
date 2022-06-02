a37337c89ba70cc5db396b845bb42d50d7b1e40a
aapk <aapk.upwork@gmail.com>
2022-06-02 12:49:43 +0300

    added 'do not disturb or snooze notifications' functionality for open and group channels

--- src/Home.js
+++ src/Home.js
@@ -25,6 +25,7 @@ const Home = () => {
           <li><Link to="/open-channel-report-a-message-user-channel">Open Channel Report a message,user or channel</Link></li>
           <li><Link to="/open-channel-categorize-messages-by-custom-type">Open Channel with Categorize messages by custom type feature</Link></li>
           <li><Link to="/open-channel-structured-data">Open Channel Structured Data</Link></li>
+          <li><Link to="/open-channel-user-do-not-disturb-or-snooze">Open Channel User do not disturb or snooze</Link></li>
         </ul>
       </div>
       <div>
@@ -52,6 +53,7 @@ const Home = () => {
           <li><Link to="/group-channel-members-list-order">Group Channel Members list order</Link></li>
           <li><Link to="/group-channel-retrieve-banned-or-muted-users">Group Channel Retrieve banned or muted users</Link></li>
           <li><Link to="/group-channel-retrieve-number-of-members-havent-read-message">Group Channel Retrieve number of members haven't read message</Link></li>
+          <li><Link to="/group-channel-user-do-not-disturb-or-snooze">Group Channel User do not disturb or snooze</Link></li>
         </ul>
       </div>
     </div>
