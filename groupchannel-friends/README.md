ceb628f8877310ea78e67b712a054f299042c85d
Luke Cha <104121286+sf-luke-cha@users.noreply.github.com>
2023-10-26 16:01:38 +0900

    Add a example for Friends

---
+++ groupchannel-friends/README.md
@@ -0,0 +1,31 @@
+# Friends in a group channel
+
+This code sample with UI components demonstrates how to add, retrieve, or remove friends in a group channel on Sendbird Chat SDK. In a group channel, the `friend` functionality serves as a tool for the current user to mark other channel members as their favorite.
+
+## Prerequisites
+
++ Node.js v10.13.0 or later
+
+## How it works
+
+`sb.createFriendListQuery()` creates a query instance for retrieving the list of friends in a group channel. 
+``` javascript
+const queryParams = { limit: 20 };
+const newQuery = sb.createFriendListQuery(queryParams);
+const userList = await newQuery.next();
+```
+
+and add/delete friends in a group channel by calling the `sb.addFriends()` and `sb.deleteFriends()` methods.
+```javascript
+await sb.addFriends([userId]);
+await sb.deleteFriend(userId);
+```
+
+## How to run
+
+Copy and paste the following code into Terminal.
+
+``` bash
+npm install
+npm start
+```
---
+++ groupchannel-friends/src/components/MessageList.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import ConfirmationModal from './ConfirmationModal';
+import {ReactComponent as Edit} from '../assets/sendbird-icon-edit.svg';
+import {ReactComponent as Delete} from '../assets/sendbird-icon-delete.svg';
+import '../styles/MessageList.css';
+
+function MessageList({ sb, channel, messageCollection, messageList, setMessageList }) {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [currentMsg, setCurrentMsg] = useState(null);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  const loadPreviousMessage = async () => {
+    if (messageCollection.hasPrevious) {
+      const prevMessageList = await messageCollection.loadPrevious();
+      const newMessageList = prevMessageList.filter(
+        (prevMsg) => !messageList.some((currentMsg) => currentMsg.messageId === prevMsg.messageId)
+      );
+      setMessageList((messageList) => [...newMessageList, ...messageList]);
+    }
+  };
+
+  const handleUpdateMessage = (message) => {
+    setIsUpdate(true);
+    setCurrentMsg(message);
+    setModalOpen(true);
+  };
+
+  const handleDeleteMessage = (message) => {
+    setIsUpdate(false);
+    setCurrentMsg(message);
+    setModalOpen(true);
+  };
+
+  const confirmUpdate = async (newMessage) => {
+    await channel.updateUserMessage(currentMsg.messageId, { message: newMessage });
+    setModalOpen(false);
+    currentMsg.message = newMessage;
+  };
+
+  const confirmDelete = async () => {
+    await channel.deleteMessage(currentMsg);
+    setModalOpen(false);
+  };
+
+  const renderMessageList = messageList.map((msg) => {
+    if (!msg.sender) return null;
+    const messageSentByMe = msg.sender?.userId === sb.currentUser.userId;
+
+    return (
+      <div key={msg.messageId} className={`message-item ${messageSentByMe ? 'message-from-you' : ''}`}>
+        <div className={`message ${messageSentByMe ? 'message-from-you' : ''}`}>
+          <div className='message-info'>
+            <div className="message-sender-name">{msg.sender?.nickname}</div>
+            <div>{new Date(msg.createdAt).toLocaleString()}</div>
+          </div>
+          {msg.messageType === "file" && (
+            <div>
+              <div>{msg.name}</div>
+              <div>{msg.plainUrl}</div>
+            </div>
+          )}
+          {msg.messageType === "user" && (
+            <div>{msg.message}</div>
+          )}
+        </div>
+        {messageSentByMe && (
+          <span style={{ marginRight: '8px' }}>
+            <Edit onClick={() => handleUpdateMessage(msg)} style={{ cursor: 'pointer', marginRight: '4px' }} />
+            <Delete onClick={() => handleDeleteMessage(msg)} style={{ cursor: 'pointer' }} />
+          </span>
+        )}
+      </div>
+    );
+  });
+
+  return (
+    <div className='message-list'>
+      {messageCollection?.hasPrevious && (
+        <div  className='load-previous-btn'>
+          <button onClick={loadPreviousMessage}>
+            Load Previous
+          </button>
+        </div>
+      )}
+      {renderMessageList}
+      {isModalOpen && (
+        <ConfirmationModal
+          isOpen={isModalOpen}
+          onRequestClose={() => setModalOpen(false)}
+          onConfirm={isUpdate ? confirmUpdate : confirmDelete}
+          title={isUpdate ? "Update Message" : "Delete Message"}
+          message={isUpdate ? currentMsg?.message : "Are you sure you want to delete this message?"}
+          isUpdateMessage={isUpdate}
+        />
+      )}
+    </div>
+  );
+}
+
+export default MessageList;
